12188eb34db3b29985d3a7bad8eca824b92fad1c
byeongjaeSon <byeongjae.dev@gmail.com>
2022-08-17 20:47:31 +0900

    refactor: Modify the solution to '42895-N으로 표현' on Programmers

--- Programmers/Programmers N으로 표현.cpp
+++ Programmers/Programmers N으로 표현.cpp
@@ -1,4 +1,3 @@
-#include <string>
 #include <vector>
 #include <unordered_set>
 using namespace std;
@@ -6,60 +5,38 @@ using namespace std;
 vector<unordered_set<int>> dp(9);
 
 int solution(int N, int number) {
-    int answer = -1;
+    int result = 0;
     for (int useCnt = 1; useCnt <= 8; useCnt++) {
         for (int a = useCnt; a >= 0; a--) {
-            int b = useCnt - a;
             if (a == useCnt) {
-                int result = 0;
+                result = 0;
                 for (int i = 0; i < useCnt; i++) result = result * 10 + N;
-                if (result == number) {
-                    answer = useCnt;
-                    break;
-                }
+                if (result == number) return useCnt;
                 dp[useCnt].insert(result);
+                continue;
             }
-            else {
-                for (auto iter1 = dp[a].begin(); iter1 != dp[a].end(); iter1++) {
-                    for (auto iter2 = dp[b].begin(); iter2 != dp[b].end(); iter2++) {
-                        int result = (*iter1) * (*iter2);
-                        if (result == number) {
-                            answer = useCnt;
-                            break;
-                        }
-                        dp[useCnt].insert(result);
-                        
-                        if (*iter2 != 0) {
-                            result = (*iter1) / (*iter2);
-                            if (result == number) {
-                                answer = useCnt;
-                                break;
-                            }
-                            dp[useCnt].insert(result);
-                        }
-                        
-                        result = (*iter1) + (*iter2);
-                        if (result == number) {
-                            answer = useCnt;
-                            break;
-                        }
-                        dp[useCnt].insert(result);
-                        
-                        result = (*iter1) - (*iter2);
-                        if (result == number) {
-                            answer = useCnt;
-                            break;
-                        }
-                        dp[useCnt].insert(result);
-                    }
-                    if (answer != -1) break;
+            int b = useCnt - a;
+            for (auto iter1 = dp[a].begin(); iter1 != dp[a].end(); iter1++) {
+                for (auto iter2 = dp[b].begin(); iter2 != dp[b].end(); iter2++) {
+                    result = (*iter1) + (*iter2);
+                    if (result == number) return useCnt;
+                    dp[useCnt].insert(result);
+
+                    result = (*iter1) - (*iter2);
+                    if (result == number) return useCnt;
+                    dp[useCnt].insert(result);
+                    
+                    result = (*iter1) * (*iter2);
+                    if (result == number) return useCnt;
+                    dp[useCnt].insert(result);
+
+                    if (*iter2 == 0) continue;
+                    result = (*iter1) / (*iter2);
+                    if (result == number) return useCnt;
+                    dp[useCnt].insert(result);                
                 }
-                if (answer != -1) break;
             }
-            if (answer != -1) break;
         }
-        if (answer != -1) break;
     }
-    
-    return answer;
-}
+    return -1;
+}}
