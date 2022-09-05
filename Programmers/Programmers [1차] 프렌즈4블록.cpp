e2c9a9224542f376fd646f0ac439a25baf36b27c
손병재 <byeongjae.dev@gmail.com>
2022-09-05 19:55:33 +0900

    Refactor: Modify the solution to '17676-[1차] 프렌즈4블록' on Programmers

--- Programmers/Programmers [1차] 프렌즈4블록.cpp
+++ Programmers/Programmers [1차] 프렌즈4블록.cpp
@@ -1,55 +1,55 @@
+#include <string>
+#include <vector>
+#include <set>
+using namespace std;
+
 int solution(int m, int n, vector<string> board) {
     int answer = 0;
-    vector<vector<bool>> isSqaure(m, vector<bool>(n, false));
-    bool canRemove = false;
-
-    do {
-        // 초기화
-        for (int i = 0; i < isSqaure.size(); i++)
-            fill(isSqaure[i].begin(), isSqaure[i].end(), false);
-        canRemove = false;
-
-        // 2x2 정사각형 찾아 기록
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 || j == 0 || board[i][j] == '0')
-                    continue;
-                if (board[i - 1][j - 1] == board[i][j - 1]
-                    && board[i - 1][j - 1] == board[i - 1][j]
-                    && board[i - 1][j - 1] == board[i][j]) {
-                    isSqaure[i][j] = true;
-                    isSqaure[i - 1][j] = true;
-                    isSqaure[i][j - 1] = true;
-                    isSqaure[i - 1][j - 1] = true;
-                    canRemove = true;
-                }
-            }
-        }
-
-        // 2x2 정사각형 다 없애기
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (isSqaure[i][j] == true) {
-                    board[i][j] = '0';
-                    answer++;
-                }
-            }
-        }
-
-        // 블록 떨어뜨려 빈공간 채우기 (두 번째 행 부터 시작)
-        for (int i = 1; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == '0') {
-                    for (int k = i; k > 0; k--) {
-                        if (board[k - 1][j] == '0')
-                            break;
-                        board[k][j] = board[k - 1][j];
-                        board[k - 1][j] = '0';
+    
+    while(1) {
+        set<pair<int,int>> s;
+        for (int i = 0; i < m-1; i++) {
+            for (int j = 0; j < n-1; j++) {
+                
+                if (board[i][j] == ' ') continue;
+                
+                vector<pair<int,int>> cmp_candidates = {{i, j+1}, {i+1, j}, {i+1, j+1}};
+                
+                bool isMatch = true;
+                for (pair<int,int>& p : cmp_candidates) {
+                    if (board[i][j] != board[p.first][p.second]){
+                        isMatch = false;
+                        break;
                     }
                 }
+                if (!isMatch) continue;
+                
+                if (s.find({i,j}) == s.end()) {
+                    s.insert({i,j});
+                }
+                
+                for (pair<int,int>& p : cmp_candidates) {
+                    if (s.find(p) == s.end()){
+                        s.insert(p);
+                    }
+                } 
             }
         }
-    } while (canRemove);
-
+        
+        for (auto e : s) {
+            board[e.first][e.second] = ' ';
+            int h = e.first;
+            while (h > 0) {
+                char tmp = board[h-1][e.second];
+                board[h-1][e.second] = board[h][e.second];
+                board[h][e.second] = tmp;
+                h--;
+            }
+        }
+        
+        answer += s.size();
+        if (s.empty()) break;
+    }
+    
     return answer;
 }
